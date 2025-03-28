1fb1377699c23a97c16fed2a5ba0e5234096563a
dxlliv <hello@dxlliv.dev>
2025-03-29 00:28:21 +0100

    refactor: Refactor core module

--- core/nuxt.config.ts
+++ core/nuxt.config.ts
@@ -1,6 +1,4 @@
-import pkg from './package.json'
 import {createResolver} from "@nuxt/kit";
-import { join } from 'path';
 
 const {resolve} = createResolver(import.meta.url);
 
@@ -9,40 +7,8 @@ export default defineNuxtConfig({
         "~": resolve(__dirname, "."),
     },
 
-    css: [
-        '~/app/assets/styles/core.scss'
-    ],
-
-    modules: [
-        '@nuxtjs/tailwindcss',
-        '@primevue/nuxt-module',
-        '@pinia/nuxt',
-        'pinia-plugin-persistedstate/nuxt',
-        '@nuxt/fonts',
-        '@vueuse/nuxt',
-        '@nuxtjs/i18n',
-        '@nuxt/icon',
-    ],
-
-    icon: {
-        clientBundle: {
-            scan: true,
-            sizeLimitKb: 256,
-        },
-    },
-
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern-compiler'
-                }
-            }
-        },
-    },
-
     imports: {
-        dirs: ['api', 'composables', 'core/controllers', 'core/managers', 'models', 'stores', 'utils'],
+        dirs: ['composables', 'core/controllers', 'core/managers', 'stores', 'utils'],
         presets: [
             {
                 from: 'nanoid',
@@ -50,19 +16,9 @@ export default defineNuxtConfig({
             },
             {
                 from: 'deepmerge',
-                imports: ['']
+                imports: ['merge']
             },
-            {
-                from: join(__dirname, 'app/utils/utilsDesktop'),
-                imports: [ 'defineDesktopApp' ]
-            }
         ]
-    },
-
-    runtimeConfig: {
-        public: {
-            coreVersion: pkg.version,
-        }
     },
 
     future: {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,25 @@
-import {owdConfig} from "./owd.config";
+import {join} from "path";
 
 export default defineNuxtConfig({
     ssr: false,
 
     extends: [
+
+        // import owd core
         './core',
-        owdConfig.theme,
+
+        // import owd theme
+        'github:owdproject/owd-theme-win95',
+
     ],
 
     css: [
         './app/assets/styles/index.scss',
     ],
+
+    i18n: {
+        strategy: 'no_prefix',
+    },
 
     future: {
         compatibilityVersion: 4,
