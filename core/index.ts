5c5634996250bcbc16f8c13cb4984e28888f5486
dxlliv <hello@dxlliv.dev>
2025-04-19 11:27:28 +0200

    Move /core/module index.ts to /core/index.ts

--- core/index.ts
+++ core/index.ts
@@ -1,3 +1,143 @@
+import {defineNuxtModule, createResolver, addComponentsDir, addImportsDir, installModule} from '@nuxt/kit'
+import pkg from '../package.json'
+
+export default defineNuxtModule({
+    meta: {
+        name: 'owd-core',
+    },
+    async setup(options, nuxt) {
+        const {resolve} = createResolver(import.meta.url);
+
+        // set core version to runtime variables
+
+        nuxt.options.runtimeConfig.public.coreVersion = pkg.version
+
+        // configure nuxt plugins
+
+        nuxt.options.primevue = nuxt.options.primevue || {};
+        nuxt.options.primevue.options = nuxt.options.primevue.options || {};
+        nuxt.options.primevue.options.theme = nuxt.options.primevue.options.theme || {};
+
+        {
+
+            // install tailwind
+
+            nuxt.options.modules.push('@nuxtjs/tailwindcss')
+            await installModule("@nuxtjs/tailwindcss", {
+                viewer: false,
+            })
+
+        }
+
+        {
+
+            // install primevue
+
+            nuxt.options.modules.push('@primevue/nuxt-module')
+            await installModule("@primevue/nuxt-module")
+
+        }
+
+        {
+
+            // install pinia
+
+            nuxt.options.modules.push('@pinia/nuxt')
+            await installModule("@pinia/nuxt")
+
+        }
+
+        {
+
+            // install @nuxt/fonts
+
+            nuxt.options.modules.push('@nuxt/fonts')
+            await installModule("@nuxt/fonts")
+
+        }
+
+        {
+
+            // install @nuxt/icon
+
+            nuxt.options.modules.push('@nuxt/icon')
+            await installModule("@nuxt/icon", {
+                clientBundle: {
+                    scan: true,
+                    sizeLimitKb: 256,
+                },
+            })
+
+        }
+
+        {
+
+            // install @vueuse/nuxt
+
+            nuxt.options.modules.push('@vueuse/nuxt')
+            await installModule("@vueuse/nuxt")
+
+        }
+
+        {
+
+            // install @nuxtjs/i18n
+
+            nuxt.options.modules.push('@nuxtjs/i18n')
+            await installModule("@nuxtjs/i18n")
+
+        }
+
+        {
+
+            // configure scss for vite
+
+            nuxt.hook('vite:extendConfig', (viteConfig) => {
+                viteConfig.css = viteConfig.css || {}
+                viteConfig.css.preprocessorOptions = viteConfig.css.preprocessorOptions || {}
+                viteConfig.css.preprocessorOptions.scss = {
+                    api: 'modern-compiler',
+                }
+            })
+
+        }
+
+        {
+
+            // add css
+
+            nuxt.options.css.push('sanitize.css')
+
+        }
+
+        {
+
+            // add runtime components
+
+            addComponentsDir({
+                path: resolve("./runtime/components"),
+                prefix: "",
+                global: true,
+            })
+
+        }
+
+        {
+
+            nuxt.options.plugins.push(
+                resolve('./runtime/plugins/resize.client.ts'),
+            )
+
+            addImportsDir(resolve('./runtime/composables'))
+            addImportsDir(resolve('./runtime/core'))
+            addImportsDir(resolve('./runtime/stores'))
+            addImportsDir(resolve('./runtime/utils'))
+
+        }
+
+    }
+})
+
 export * from './runtime/composables/useAppEntries'
 export * from './runtime/composables/useApplicationManager'
 export * from './runtime/composables/useApplicationState'
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
     },
 
     modules: [
-        './core/module',
+        './core',
     ],
 
     extends: [
