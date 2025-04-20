cd237740efb81de03b409131d783825f15ac84b8
dxlliv <hello@dxlliv.dev>
2025-04-20 18:45:22 +0200

    Reset main owd.config.ts

--- owd.config.ts
+++ owd.config.ts
@@ -2,9 +2,6 @@ import {defineDesktopConfig} from "./core/runtime/utils/utilsDesktop"
 
 export default defineDesktopConfig({
     apps: [
-        './dev/apps/app-about',
-        './dev/apps/app-todo',
-        './dev/apps/app-debug',
-        './dev/apps/app-wasmboy',
+        'app-about',
     ]
 })
