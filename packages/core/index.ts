833ba6622f5877d71c7ef6045fa80e5674e2fc4b
dxlliv <dxlliv@proton.me>
2026-06-15 03:54:55 +0200

    refactor: convert core and cli to Git submodules

--- packages/core/index.ts
+++
@@ -1,8 +0,0 @@
-export { defineDesktopApp } from './kit/defineDesktopApp'
-export {
-  defineDesktopConfig,
-  defineDesktopModule,
-  defineDesktopTheme,
-  mergeDesktopExtensionConfig,
-  setDesktopExtensionConfig,
-} from './kit/authoring'
