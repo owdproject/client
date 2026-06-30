6f7af0fbebc5f9adb004c0103cbe05cdcbe29c02
dxlliv <hello@dxlliv.dev>
2026-06-30 23:47:48 +0200

    refactor: clean up test desktop configuration and prune redundant dependencies

--- desktop/desktop.config.ts
+++ desktop/desktop.config.ts
@@ -1,7 +1,7 @@
 import { defineDesktopConfig } from '@owdproject/core'
 
 export default defineDesktopConfig({
-  theme: '@owdproject/theme-win95',
-  apps: [ '@owdproject/app-about' ],
-  modules: [  ],
+  theme: '@owdproject/theme-nova',
+  apps: ['@owdproject/app-about'],
+  modules: [],
 })
